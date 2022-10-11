3c3b417a2edfe96ee0965024b7918a5b9515a873
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-10-11 22:01:24 +0530

    Add files via upload

---
+++ ASSIGNMENT_7/A_7 Q7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+ printf("enter a number  : ");
+ scanf("%d",&a);
+ if(a%5==0){
+ 	printf("%d is is divisible by 5",a);
+ }
+ else
+ 	printf("%d is  not divisible by 5",a);
+return 0;
+}
