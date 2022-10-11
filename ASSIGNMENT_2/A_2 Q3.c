511f682520babee343879ccee6503ae1281073d7
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-10-11 21:52:54 +0530

    Add files via upload

---
+++ ASSIGNMENT_2/A_2 Q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int aditya;  //ROLLL NO. OF TWO STUDENTS ADITYA AND PRATEEK
+	aditya=10;
+	int prateek;
+	prateek=20;
+	printf("roll no. of aditya is - %d \n",aditya);
+	printf("roll no. of prateek is - %d", prateek);
+	return 0;
+}
---
+++ ASSIGNMENT_2/A_2 Q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{                                 // ROLL NO. OF ADITYA,PRATEEK,CHIRAG
+	int aditya,prateek,chirag;
+	aditya=5;
+	prateek=6;
+	chirag=7;
+	printf("roll no. of aditya is - %d roll no. of prateek is - %d roll no. of chirag is - %d",aditya,prateek,chirag);
+	return 0;
+}
