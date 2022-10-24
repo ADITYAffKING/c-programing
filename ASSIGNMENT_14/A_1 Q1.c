50a96fd396972c4b9706eadf7f1c1b4bf06235f7
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-10-24 23:23:54 +0530

    Add files via upload

---
+++ ASSIGNMENT_14/A_1 Q1.c
@@ -0,0 +1,20 @@
+/*
+AUTHOR: ADITYA CHHIPA
+DATE :28/09/22
+AIM: print Hello
+LANGUAGE: C
+*/
+#include <stdio.h>
+void hello();
+int main()
+{
+	 hello();
+	
+	return 0;
+}
+void hello(){
+	
+printf("hello");
+
+}
+	
