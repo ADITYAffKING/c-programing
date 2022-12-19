d4337822c55f7237e7f6cbaa5f8c2b9e3f18674a
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-12-19 22:41:28 +0530

    Update A21_Q1.c

--- assignment_21/A21_Q1.c
+++ assignment_21/A21_Q1.c
@@ -2,9 +2,11 @@
 #include<string.h>
 int main(){
     char str[100];
-    int a;
+    int a,i;
     printf("ENTER STRING : ");
     fgets(str,100,stdin);
-    a=strlen(str);
-    printf("%d",a-1);
+//     a=strlen(str);
+    for(i=0;str[i]!='\0';i++){
+    }
+    printf("%d",i-1);
 }
