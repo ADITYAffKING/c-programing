fc13420a690e08d4df4f29ce581ddc5eab47aa3b
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-12-20 22:17:49 +0530

    Update A21_Q2.c

--- assignment_21/A21_Q2.c
+++ assignment_21/A21_Q2.c
@@ -1,17 +1,33 @@
 #include<string.h>
 #include<stdio.h>
+int compare(char str1[],char str2[]);
 int main(){
     char str1[100],str2[100];
     printf("ENTER 1ST STRING : ");
     fgets(str1,100,stdin);
     printf("ENTER 2ND STRING : ");
     fgets(str2,100,stdin);
-    int d=strcmp(str1,str2);
     
-    if(d==0){
-        printf("true");
+    int check=compare(str1,str2);
+    
+    switch(check){
+
+        case 0 : printf("strings are not equal ");
+                 break;
+        default : printf("strings are equal ");
+                 break;
+
     }
-    else{
-        printf("false");
-    }
+}
+int compare(char str1[],char str2[]){
+  for(int i=0;str1[i-1]!='\0';i++){
+   if(str1[i]!=str2[i]){
+    
+    return 0;
+   
+   }
+   if(str1[i]=='\0'){
+       return 1;
+   }
+  }
 }
