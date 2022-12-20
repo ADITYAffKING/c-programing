fa386ddab535160d91e0700c02cf043f0b698a20
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-12-20 23:05:47 +0530

    Update A21_Q6.c

--- assignment_21/A21_Q6.c
+++ assignment_21/A21_Q6.c
@@ -1,9 +1,26 @@
 #include<stdio.h>
-#include<string.h>
+void strREVERSE(char str[]);
 int main(){
     char str[100];
     printf("enter a string : ");
     fgets(str,100,stdin);
-    strrev(str);
+    
+    strREVERSE(str);
+
+    
+}
+void strREVERSE(char str[]){
+    int i,j,k;
+    char store;
+
+    for(i=0;str[i+1]!='\0';i++){
+    }
+    k=i/2;
+
+    for(j=0;j<k;j++){
+        store=str[i-j-1];
+        str[i-j-1]=str[j];
+        str[j]=store;
+    }
     puts(str);
 }
