f24d3228e5b45c2f1b30ae7cc755104d6bf12f8d
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-12-20 22:15:57 +0530

    Update A21_Q10.c

--- assignment_21/A21_Q10.c
+++ assignment_21/A21_Q10.c
@@ -1,16 +1,33 @@
-#include<stdio.h>
 #include<string.h>
+#include<stdio.h>
+int compare(char str1[],char str2[]);
 int main(){
-    char  str[100];
-    int count=1,a;
-    printf("ENTER YOUR STRING : ");
-    fgets(str,100,stdin);
-    a=strlen(str)-1;
-    for(int i=1;str[i]!='\0';i++){
+    char str1[100],str2[100];
+    printf("ENTER 1ST STRING : ");
+    fgets(str1,100,stdin);
+    printf("ENTER 2ND STRING : ");
+    fgets(str2,100,stdin);
+    
+    int check=compare(str1,str2);
+    
+    switch(check){
 
-        if(str[i]==32&&i!=0&&i!=a&&str[i+1]!=32){
-            count++;
-        }
+        case 0 : printf("strings are not equal ");
+                 break;
+        default : printf("strings are equal ");
+                 break;
+
     }
-     printf("number of words are : %d",count);
+}
+int compare(char str1[],char str2[]){
+  for(int i=0;str1[i-1]!='\0';i++){
+   if(str1[i]!=str2[i]){
+    
+    return 0;
+   
+   }
+   if(str1[i]=='\0'){
+       return 1;
+   }
+  }
 }
