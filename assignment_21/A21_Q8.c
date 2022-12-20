685908eb3a3c218737ed32dbfc43b9307853332e
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-12-20 23:29:55 +0530

    Update A21_Q8.c

--- assignment_21/A21_Q8.c
+++ assignment_21/A21_Q8.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
-#include<string.h>
+void stringcopy(char str1[]);
 int main(){
-    char str1[100],str2[100];
+
+    char str1[100];
     printf("enter string : ");
     fgets(str1,100,stdin);
-    strcpy(str2,str1);
-    puts(str2);
+    stringcopy(str1);
+ 
+}
+void stringcopy(char str1[]){
+      
+       char str2[100];
+        int i;
+       for(i=0;str1[i]!='\0';i++){
+
+          str2[i]=str1[i];
+       }
+    
+        puts(str2);
+
 }
