883fa0fe5c6e2769f66bde8e89ea76725becc057
ADITYAffKING <115412743+ADITYAffKING@users.noreply.github.com>
2022-12-20 22:20:52 +0530

    Update A21_Q3.c

--- assignment_21/A21_Q3.c
+++ assignment_21/A21_Q3.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
     char str[100];
     int count=0;
     printf("enter a string : ");
     fgets(str,100,stdin);
-    strupr(str);
     for(int i=0;str[i]!=0;i++){
-      if(str[i]=='A'||str[i]=='U'||str[i]=='O'||str[i]=='I'||str[i]=='E'){
+      if(str[i]=='A'||str[i]=='U'||str[i]=='O'||str[i]=='I'||str[i]=='E'||str[i]=='o'||str[i]=='i'||str[i]=='a'||str[i]=='e'||str[i]=='u'){
         count++;
       }
     }
